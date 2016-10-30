e92e6538bc895144f09454cd80597b1da66c4aea
Tephy-Espin-Arroba <slespin@espe.edu.ec>
2016-10-30 18:06:26 -0500

    Assigment2
    Inverse of a square matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Fist, the funtion will generate a cuadratic matrix
+  #then it will store the original and the inverse matrix in variables
+  
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+  }
+  
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  #this funtion will store the inverse result
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #The function will receive the matrix, then, show the inverse if it is already obtained
+  #or it will pass to the solve function
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  m <- x$get()
+  inv <- solve(m, ...)
+  #the function solve the inverse from the variable m, defined from the x variable in the   
+  #previous function
+  x$setInverse(inv)
+  inv
 }
+
+#sea
